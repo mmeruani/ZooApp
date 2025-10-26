536c9dd44e1967c97b609b1bafdb0faa5a720f61
Maria Medvedskaya <mariamedvedskaa0@gmail.com>
2025-10-26 19:58:23 +0300

    Update Animal.cs

--- ZooApp/Domain/Animals/Animal.cs
+++ ZooApp/Domain/Animals/Animal.cs
@@ -1,6 +1,16 @@
+using ZooApp.Domain.Abstractions;
 namespace ZooApp.Domain.Animals;
 
-public class Animal
+public abstract class Animal : IInventory, IAlive
 {
+    public int Number { get; init; }
+    public string Name { get; init; }
+    public int Food { get; init; }      
     
+    protected Animal(string name, int number, int food)
+    {
+        Name = name;
+        Number = number;
+        Food = food;
+    }
 }
