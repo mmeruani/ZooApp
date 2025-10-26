0a20b57024611d0744169a04d0987d8e7ef4bc9d
Maria Medvedskaya <mariamedvedskaa0@gmail.com>
2025-10-26 20:04:59 +0300

    Update InMemoryZooRepository.cs

--- ZooApp/Infrastructure/Persistence/InMemoryZooRepository.cs
+++ ZooApp/Infrastructure/Persistence/InMemoryZooRepository.cs
@@ -1,6 +1,34 @@
+using ZooApp.Application.Abstractions;
+using ZooApp.Domain.Animals;
+using ZooApp.Domain.Abstractions;
+using ZooApp.Domain.Things;
+
 namespace ZooApp.Infrastructure.Persistence;
 
-public class InMemoryZooRepository
+public class InMemoryZooRepository : IZooRepository
 {
+    private readonly List<Animal> _animals = new();
+    private readonly List<IInventory> _inventory = new();
+
+    public void AddAnimal(Animal animal)
+    {
+        if (animal is null)
+        {
+            throw new ArgumentNullException(nameof(animal));
+        }
+        _animals.Add(animal);
+        _inventory.Add(animal);
+    }
     
+    public void AddThing(Thing thing)
+    {
+        if (thing is null)
+        {
+            throw new ArgumentNullException(nameof(thing));
+        }    
+        _inventory.Add(thing);
+    }
+    
+    public IReadOnlyList<Animal> GetAnimals() => _animals.AsReadOnly();
+    public IReadOnlyList<IInventory> GetInventory() => _inventory.AsReadOnly();
 }
