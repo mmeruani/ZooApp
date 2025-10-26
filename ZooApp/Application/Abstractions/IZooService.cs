fb8c5687f686c246df0fe05e4b73ec30c8be0e3d
Maria Medvedskaya <mariamedvedskaa0@gmail.com>
2025-10-26 19:55:56 +0300

    Update IZooService.cs

--- ZooApp/Application/Abstractions/IZooService.cs
+++ ZooApp/Application/Abstractions/IZooService.cs
@@ -1,6 +1,15 @@
+using ZooApp.Domain.Animals;
+using ZooApp.Domain.Things;
+using ZooApp.Domain.Abstractions;
+
 namespace ZooApp.Application.Abstractions;
 
 public interface IZooService
 {
-    
+    bool TryAcceptAnimal(Animal animal);
+    void AddThing(Thing thing);
+    int GetAnimalsCount();
+    int GetTotalFoodPerDay();
+    IEnumerable<Herbo> GetPettingZooCandidates();
+    IEnumerable<IInventory> GetInventoryReport();    
 }
