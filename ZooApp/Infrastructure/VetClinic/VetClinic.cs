61e486810960965aeee4e653e3128c9e0947a14e
Maria Medvedskaya <mariamedvedskaa0@gmail.com>
2025-10-26 20:05:23 +0300

    Update VetClinic.cs

--- ZooApp/Infrastructure/VetClinic/VetClinic.cs
+++ ZooApp/Infrastructure/VetClinic/VetClinic.cs
@@ -1,6 +1,10 @@
+using ZooApp.Application.Abstractions;
+using ZooApp.Domain.Animals;
+
 namespace ZooApp.Infrastructure.VetClinic;
 
-public class VetClinic
+public class VetClinic : IVetClinic
 {
-    
+    private static readonly Random Rnd = new();
+    public bool IsHealthy(Animal _) => Rnd.Next(0, 100) < 80;     // вероятность, что здоров > 0,8
 }
