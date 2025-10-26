66f655a9a9e36362226e7e643adb4b99b70c70c0
Maria Medvedskaya <mariamedvedskaa0@gmail.com>
2025-10-26 19:59:07 +0300

    Update Monkey.cs

--- ZooApp/Domain/Animals/Monkey.cs
+++ ZooApp/Domain/Animals/Monkey.cs
@@ -1,6 +1,7 @@
 namespace ZooApp.Domain.Animals;
 
-public class Monkey
+public class Monkey : Herbo
 {
-    
+    public Monkey(string name, int number, int food, int kindness)
+        : base(name, number, food, kindness) {}
 }
