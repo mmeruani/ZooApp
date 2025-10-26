54bdb558f5be145ee530cd1d4bf5d44b644cc646
Maria Medvedskaya <mariamedvedskaa0@gmail.com>
2025-10-26 19:55:34 +0300

    Update IZooRepository.cs

--- ZooApp/Application/Abstractions/IZooRepository.cs
+++ ZooApp/Application/Abstractions/IZooRepository.cs
@@ -1,6 +1,12 @@
+using ZooApp.Domain.Abstractions;
+using ZooApp.Domain.Animals;
+using ZooApp.Domain.Things;
 namespace ZooApp.Application.Abstractions;
 
 public interface IZooRepository
 {
-    
+    void AddAnimal(Animal animal);
+    void AddThing(Thing thing);
+    IReadOnlyList<Animal> GetAnimals();
+    IReadOnlyList<IInventory> GetInventory();    
 }
