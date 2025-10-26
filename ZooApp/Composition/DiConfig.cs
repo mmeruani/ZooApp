f193bcf4817a04309513192cd18ca15ccbc79b18
Maria Medvedskaya <mariamedvedskaa0@gmail.com>
2025-10-26 19:57:06 +0300

    Update DiConfig.cs

--- ZooApp/Composition/DiConfig.cs
+++ ZooApp/Composition/DiConfig.cs
@@ -1,6 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using ZooApp.Application.Abstractions;
+using ZooApp.Application.Services;
+using ZooApp.Infrastructure.Persistence;
+using ZooApp.Infrastructure.VetClinic;
+using ZooApp.Presentation;
+
 namespace ZooApp.Composition;
 
-public class DiConfig
+public static class DiConfig
 {
-    
+    public static IServiceProvider Build()
+    {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<IVetClinic, VetClinic>();
+        services.AddSingleton<IZooRepository, InMemoryZooRepository>();
+        services.AddSingleton<IZooService, ZooService>();
+
+        services.AddSingleton<ConsoleApp>();
+
+        return services.BuildServiceProvider();
+    }
 }
