14c5587c9932869746b59cc5e9ea1a54a2de6803
Maria Medvedskaya <mariamedvedskaa0@gmail.com>
2025-10-26 20:04:28 +0300

    Update Thing.cs

--- ZooApp/Domain/Things/Thing.cs
+++ ZooApp/Domain/Things/Thing.cs
@@ -1,6 +1,14 @@
+using ZooApp.Domain.Abstractions;
 namespace ZooApp.Domain.Things;
 
-public class Thing
+public abstract class Thing : IInventory
 {
+    public int Number { get; init; }   
+    public string Name { get; init; }
     
+    protected Thing(string name, int number)
+    {
+        Name = name;
+        Number = number;
+    }
 }
